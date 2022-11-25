4455cf116dd64205706775a1a5d5c2b65f60c8c1
j-mendez <jeff@a11ywatch.com>
2022-11-24 21:46:13 -0500

    build(chrome): add driver control setup

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # chrome
 
 Chrome cross compiled dockerfile with tini startup
+
+## Features
+
+1. Can spawn multiple chrome instances.
+1. Get chrome ws connections and status.
+
+
+The current instance binds chrome to 0.0.0.0 when starting via API.
+
+Use the env variable `REMOTE_ADDRESS` to change the address of the chrome instance between physical or network.
+
+## Todo
+
+1. Control chrome instances via http to spin up and down.
+2. opt to build chrome direct without docker.
---
+++ src/main.rs
@@ -0,0 +1,96 @@
+use hyper::{Body, Client, Method, Request};
+use warp::{Filter, Rejection, Reply};
+use std::process::Command;
+
+type Result<T> = std::result::Result<T, Rejection>;
+
+/// static chrome arguments to start application
+static CHROME_ARGS: [&'static str; 38] = [
+    "--headless",
+    "--no-sandbox",
+    "--no-first-run",
+    "--remote-debugging-address=0.0.0.0",
+    "--remote-debugging-port=9222",
+    "--max-wait-for-load=2500",
+    "--hide-scrollbars",
+    "--mute-audio",
+    "--allow-running-insecure-content",
+    "--autoplay-policy=user-gesture-required",
+    "--ignore-certificate-errors",
+    "--no-default-browser-check",
+    "--metrics-recording-only",
+    "--disable-default-apps",
+    "--disable-storage-reset",
+    "--disable-dev-shm-usage",
+    "--disable-http2",
+    "--disable-domain-reliability",
+    "--disable-component-update",
+    "--disable-sync",
+    "--disable-background-networking",
+    "--disable-background-timer-throttling",
+    "--disable-notifications",
+    "--disable-accelerated-2d-canvas",
+    "--disable-accelerated-video-decode",
+    "--disable-extensions",
+    "--disable-popup-blocking",
+    "--disable-renderer-backgrounding",
+    "--disable-client-side-phishing-detection",
+    "--disable-setuid-sandbox",
+    "--disable-hang-monitor",
+    "--disable-features=ScriptStreaming,TranslateUI,BlinkGenPropertyTrees",
+    "--disable-backgrounding-occluded-windows",
+    "--disable-component-extensions-with-background-pages",
+    "--disable-renderer-backgrounding",
+    "--disable-threaded-animation",
+    "--disable-threaded-compositing",
+    "--enable-background-thread-pool",
+];
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    let base_route = warp::path::end().and_then(handler);
+    let fork = warp::path("fork").map(|| {
+        let mut command = Command::new("chromium-browser");
+
+        let id = if let Ok(child) = command
+            .args(CHROME_ARGS)
+            .spawn()
+        {
+            let cid = child.id();
+            println!("CID is {}", child.id());
+
+            cid
+        } else {
+            println!("chrome command didn't start");
+
+            0
+        }
+        .to_string();
+
+        id
+    });
+
+    let ctrls = warp::post().and(fork.with(warp::cors().allow_any_origin()));
+
+    let routes = warp::get()
+        .and(base_route.with(warp::cors().allow_any_origin()))
+        .or(ctrls);
+
+    println!("Chrome server at localhost:6000");
+    warp::serve(routes).run(([0, 0, 0, 0], 6000)).await;
+}
+
+/// get json endpoint for chrome instance
+async fn handler() -> Result<impl Reply> {
+    let req = Request::builder()
+        .method(Method::GET)
+        .uri("http://127.0.0.1:9222/json/version")
+        .header("content-type", "application/json")
+        .body(Body::default())
+        .unwrap_or_default();
+
+    let client = Client::new();
+    let resp = client.request(req).await.unwrap_or_default();
+
+    Ok(resp)
+}
