49cf1a55400ff5e72cc1159b58aff7d99b1f7a36
j-mendez <jeff@a11ywatch.com>
2022-11-29 10:33:31 -0500

    build(flags): fix multi background devtools usage

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,11 +4,12 @@ set -e
 
 REMOTE_ADDRESS="${REMOTE_ADDRESS:-127.0.0.1}";
 
-exec chrome_driver & chromium-browser --headless --disable-software-rasterizer --no-sandbox --disable-gpu --hide-scrollbars --mute-audio --no-first-run \
-    --remote-debugging-address=$REMOTE_ADDRESS --remote-debugging-port=9222 --max-wait-for-load=2500 --allow-running-insecure-content --autoplay-policy=user-gesture-required \
-    --disable-default-apps --disable-storage-reset --disable-dev-shm-usage --disable-component-update --disable-sync --disable-background-networking \
-    --disable-background-timer-throttling --disable-notifications --disable-accelerated-2d-canvas --disable-accelerated-video-decode --disable-extensions \
-    --disable-popup-blocking --disable-renderer-backgrounding --disable-client-side-phishing-detection --disable-setuid-sandbox \
-    --disable-features=TranslateUI BlinkGenPropertyTrees --ignore-certificate-errors --disable-http2 --disable-backgrounding-occluded-windows --no-default-browser-check \
-    --metrics-recording-only --disable-component-extensions-with-background-pages --disable-threaded-animation \
-    --disable-threaded-compositing --enable-background-thread-pool
+exec chrome_driver & chromium-browser --headless --no-sandbox --hide-scrollbars --mute-audio --no-first-run \
+    --remote-debugging-address=$REMOTE_ADDRESS --remote-debugging-port=9222 --max-wait-for-load=2500 --allow-running-insecure-content \
+    --autoplay-policy=user-gesture-required --enable-background-thread-pool --disable-gpu --disable-software-rasterizer \
+    --disable-default-apps --disable-storage-reset --disable-dev-shm-usage --disable-sync \
+    --disable-notifications --disable-accelerated-video-decode \
+    --disable-client-side-phishing-detection --disable-setuid-sandbox \
+    --disable-features=TranslateUI BlinkGenPropertyTrees --ignore-certificate-errors --no-default-browser-check \
+    --metrics-recording-only --disable-threaded-animation
+    
