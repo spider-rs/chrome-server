5d0196f02ca841c03469c25da29b330364cd46b8
j-mendez <jeff@a11ywatch.com>
2023-02-08 18:41:03 -0500

    build(chrome): fix cdp entry

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,9 +5,9 @@ set -e
 REMOTE_ADDRESS="${REMOTE_ADDRESS:-127.0.0.1}";
 LOG_ENABLED=${LOG_ENABLED:-0};
 
-exec chrome_driver & chromium-browser --headless=new --no-sandbox --hide-scrollbars --no-first-run --disable-sync --enable-automation --disable-popup-blocking --disable-prompt-on-repost \
-    --remote-debugging-address=$REMOTE_ADDRESS --remote-debugging-port=9222 --max-wait-for-load=2500 --allow-running-insecure-content --metrics-recording-only --allow-pre-commit-input --disable-background-networking \
+exec chrome_driver & chromium-browser --headless --no-sandbox --hide-scrollbars --no-first-run --disable-sync --enable-automation --disable-popup-blocking --disable-prompt-on-repost \
+    --remote-debugging-address=$REMOTE_ADDRESS --remote-debugging-port=9222 --allow-running-insecure-content --metrics-recording-only --allow-pre-commit-input --disable-background-networking \
     --autoplay-policy=user-gesture-required --enable-background-thread-pool --disable-gpu --disable-software-rasterizer --disable-ipc-flooding-protection --mute-audio --disable-client-side-phishing-detection \
     --disable-storage-reset --disable-background-timer-throttling --disable-dev-shm-usage --disable-backgrounding-occluded-windows --disable-partial-raster --disable-breakpad --disable-default-apps --enable-logging=$LOG_ENABLED \
-    --disable-accelerated-video-decode --disable-setuid-sandbox --disable-domain-reliability --no-pings --disable-hang-monitor --disable-extensions --user-data-dir=/usr/src/app --force-fieldtrials=*BackgroundTracing/default/ \
+    --disable-accelerated-video-decode --disable-setuid-sandbox --disable-domain-reliability --no-pings --disable-hang-monitor --disable-extensions --force-fieldtrials=*BackgroundTracing/default/ \
     --disable-features=BackForwardCache,AcceptCHFrame,AvoidUnnecessaryBeforeUnloadCheckSync,Translate,InterestFeedContentSuggestions,BlinkGenPropertyTrees,MediaRouter,OptimizationHints --ignore-certificate-errors --disable-component-extensions-with-background-pages
