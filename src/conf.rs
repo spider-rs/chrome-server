94a5dea9a0d1571114218b0feb0304fc1c574f75
j-mendez <jeff@a11ywatch.com>
2024-09-03 05:31:09 -0400

    chore(conf): revert site-per-process removal

--- src/conf.rs
+++ src/conf.rs
@@ -121,7 +121,7 @@ lazy_static! {
             "--export-tagged-pdf",
             "--no-pings",
             "--use-gl=swiftshader",
-            "--window-size=1920,1080",
+            "--window-size=1280,720",
             "--disable-vulkan-fallback-to-gl-for-testing",
             "--disable-vulkan-surface",
             "--disable-features=AudioServiceOutOfProcess,IsolateOrigins,site-per-process,ImprovedCookieControls,LazyFrameLoading,GlobalMediaControls,DestroyProfileOnBrowserClose,MediaRouter,DialMediaRouteProvider,AcceptCHFrame,AutoExpandDetailsElement,CertificateTransparencyComponentUpdater,AvoidUnnecessaryBeforeUnloadCheckSync,Translate"
